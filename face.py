cd802b389d9a9a40a52049a68a22ebfc28db31d9
Venkata siva chandu <139946397+chandu080201@users.noreply.github.com>
2023-10-14 09:36:34 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,31 @@
+import cv2
+
+# Load the pre-trained face detection cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize the video capture object
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame from the video capture
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(35, 35))
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 275, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Face Detection', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the windows
+cap.release()
+cv2.destroyAllWindows()
